aace89bb6abaedf189be7ea9250f1e6799acaf8c
Sylvia Yanes <syanes@uw.edu>
2022-05-28 12:39:11 -0700

    add modal

--- photos.js
+++ photos.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+$("img").attr("data-bs-toggle", "modal");
+$("img").attr("data-bs-target", "#pop");
+
+$(".modal").on("show.bs.modal", function (event) {
+    let imageClicked = $(event.relatedTarget);
+    let srcAttribute = imageClicked.attr("src");
+    let altAttribute = imageClicked.attr("alt");
+    $("#modalImage").attr("src", srcAttribute);
+    $("#modalImage").attr("alt", altAttribute);
+})
+
